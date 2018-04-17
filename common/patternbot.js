72c655d39d0d7291db8f7d23deb6b57fc3344b69
ethto <estherfjohnson@gmail.com>
2018-04-17 12:12:15 -0400

    Change Home to Details for Product Details title

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/estherjohnson/Documents/Algonquin/Semester 4/Web Dev IV/ecommerce-pattern-library
- * @version 1523981360110
+ * @version 1523981512436
  */
-const patternManifest_1523981360110 = {
+const patternManifest_1523981512436 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -1059,5 +1059,5 @@ const patternManifest_1523981360110 = {
   }
 };
 
-patternBotIncludes(patternManifest_1523981360110);
+patternBotIncludes(patternManifest_1523981512436);
 }());
